d4203068c31ee63f0ac962b64bcd0064b8deedcb
Apostol Apostolov <a.apostolov45@gmail.com>
2021-11-15 20:52:28 +0000

    deleted unused code

--- Musk_Inspections/Create_report_page.cs
+++ Musk_Inspections/Create_report_page.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 using System.Reflection;
 using Word = Microsoft.Office.Interop.Word;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 
 namespace Musk_Inspections
@@ -27,34 +18,7 @@ namespace Musk_Inspections
         {
             InitializeComponent();
         }
-        private void FindAndReplace(Word.Application wordApp, object ToFindText, object replaceWithText)
-        {
-            object matchCase = true;
-            object matchWholeWord = true;
-            object matchWildCards = false;
-            object matchSoundLike = false;
-            object nmatchAllforms = false;
-            object forward = true;
-            object format = false;
-            object matchKashida = false;
-            object matchDiactitics = false;
-            object matchAlefHamza = false;
-            object matchControl = false;
-            object read_only = false;
-            object visible = true;
-            object replace = 2;
-            object wrap = 1;
-
-            wordApp.Selection.Find.Execute(ref ToFindText,
-                ref matchCase, ref matchWholeWord,
-                ref matchWildCards, ref matchSoundLike,
-                ref nmatchAllforms, ref forward,
-                ref wrap, ref format, ref replaceWithText,
-                ref replace, ref matchKashida,
-                ref matchDiactitics, ref matchAlefHamza,
-                ref matchControl);
-        }
-                        
+                     
 
 
         private void button1_Click(object sender, EventArgs e)
@@ -134,52 +98,10 @@ namespace Musk_Inspections
         {
 
         }
-        private void CreateWordDocument(object filename, object SaveAs)
-        {
-            Word.Application wordApp = new Word.Application();
-            object missing = Missing.Value;
-            Word.Document myWordDoc = null;
-
-            if (File.Exists((string)filename))
-            {
-                object readOnly = false;
-                object isVisible = false;
-                wordApp.Visible = false;
-
-                myWordDoc = wordApp.Documents.Open(ref filename, ref missing, ref readOnly,
-                                        ref missing, ref missing, ref missing,
-                                        ref missing, ref missing, ref missing,
-                                        ref missing, ref missing, ref missing,
-                                        ref missing, ref missing, ref missing, ref missing);
-                myWordDoc.Activate();
-
-                //find and replace
-                this.FindAndReplace(wordApp, "<name>", textBox1.Text);
-                this.FindAndReplace(wordApp, "<workArea>", textBox2.Text);
-                this.FindAndReplace(wordApp, "<site>", textBox3.Text);
-                this.FindAndReplace(wordApp, "<jobDescription>", textBox4.Text);
-                this.FindAndReplace(wordApp, "<type>", textBox5.Text);
-                this.FindAndReplace(wordApp, "<feedback>", textBox6.Text);               
-                this.FindAndReplace(wordApp, "<date>", DateTime.Now.ToShortDateString());
-            }
-            else
-            {
-                MessageBox.Show("File not Found!");
-            }
-            myWordDoc.SaveAs2(ref SaveAs, ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing);
-
-            myWordDoc.Close();
-            wordApp.Quit();
-            MessageBox.Show("File Created!");
-        }
-
+        
         private void button3_Click(object sender, EventArgs e)
         {
-            CreateWordDocument(@"C:\Developement\Musk_Inspection\Temp.docx", @"C:\Developement\Musk_Inspection\MuskReport.docx");
+            
         }
 
        
