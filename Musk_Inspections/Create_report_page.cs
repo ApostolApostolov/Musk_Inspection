af67553bd27f388b87894e5246f50602862f1922
Apostol Apostolov <a.apostolov45@gmail.com>
2021-11-21 19:14:38 +0000

    data in database

--- Musk_Inspections/Create_report_page.cs
+++ Musk_Inspections/Create_report_page.cs
@@ -19,6 +19,7 @@ namespace Musk_Inspections
             InitializeComponent();
         }
                      
+        //database push
 
 
         private void button1_Click(object sender, EventArgs e)
