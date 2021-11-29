b12d0a0a0cc09634b991e4b458180e5ddb15d3bf
Alex Brain <abrain5423@gmail.com>
2021-11-29 14:55:21 +0000

    Login Code to Check Valid Info and load Dashboard

--- Musk_Inspections/LoginPage.Designer.cs
+++ Musk_Inspections/LoginPage.Designer.cs
@@ -39,64 +39,72 @@
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(566, 439);
+            this.btnLogin.Location = new System.Drawing.Point(424, 357);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Size = new System.Drawing.Size(56, 19);
             this.btnLogin.TabIndex = 0;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
             // 
             // lbUsername
             // 
             this.lbUsername.AutoSize = true;
-            this.lbUsername.Location = new System.Drawing.Point(325, 333);
+            this.lbUsername.Location = new System.Drawing.Point(244, 271);
+            this.lbUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbUsername.Name = "lbUsername";
-            this.lbUsername.Size = new System.Drawing.Size(73, 17);
+            this.lbUsername.Size = new System.Drawing.Size(55, 13);
             this.lbUsername.TabIndex = 1;
             this.lbUsername.Text = "Username";
             // 
             // lbPassword
             // 
             this.lbPassword.AutoSize = true;
-            this.lbPassword.Location = new System.Drawing.Point(325, 376);
+            this.lbPassword.Location = new System.Drawing.Point(244, 306);
+            this.lbPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbPassword.Name = "lbPassword";
-            this.lbPassword.Size = new System.Drawing.Size(69, 17);
+            this.lbPassword.Size = new System.Drawing.Size(53, 13);
             this.lbPassword.TabIndex = 2;
             this.lbPassword.Text = "Password";
             // 
             // tbUsername
             // 
-            this.tbUsername.Location = new System.Drawing.Point(439, 328);
+            this.tbUsername.Location = new System.Drawing.Point(329, 266);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(2);
             this.tbUsername.Name = "tbUsername";
-            this.tbUsername.Size = new System.Drawing.Size(100, 22);
+            this.tbUsername.Size = new System.Drawing.Size(76, 20);
             this.tbUsername.TabIndex = 3;
             // 
             // tbPassword
             // 
-            this.tbPassword.Location = new System.Drawing.Point(439, 376);
+            this.tbPassword.Location = new System.Drawing.Point(329, 306);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(2);
             this.tbPassword.Name = "tbPassword";
-            this.tbPassword.Size = new System.Drawing.Size(100, 22);
+            this.tbPassword.Size = new System.Drawing.Size(76, 20);
             this.tbPassword.TabIndex = 4;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(268, 39);
+            this.pictureBox1.Location = new System.Drawing.Point(201, 32);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(373, 216);
+            this.pictureBox1.Size = new System.Drawing.Size(280, 176);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // LoginPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(934, 615);
+            this.ClientSize = new System.Drawing.Size(700, 500);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.tbPassword);
             this.Controls.Add(this.tbUsername);
             this.Controls.Add(this.lbPassword);
             this.Controls.Add(this.lbUsername);
             this.Controls.Add(this.btnLogin);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "LoginPage";
             this.Text = "Musk";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
--- Musk_Inspections/LoginPage.cs
+++ Musk_Inspections/LoginPage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,38 @@ namespace Musk_Inspections
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private static Boolean CheckLogin(string username, string password)
+        {
+            using (SqlConnection cn = new SqlConnection(Properties.Settings.Default.DB_MUSK))
+            {
+                cn.Open();
+
+                try
+                {
+                    SqlDataAdapter data = new SqlDataAdapter(new SqlCommand("SELECT Pwd FROM Inspector WHERE Inspector_id = " + username + " AND Pwd = " + password));
+
+                    DataTable table = new DataTable();
+
+                    data.Fill(table);
+
+                    return table.Rows.Count > 0 ? true : false;
+                }
+                catch (Exception e) { return false; }
+            }
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            if (CheckLogin(tbUsername.Text, tbPassword.Text))
+            {
+                this.Hide();
+                Dashboard dash = new Dashboard();
+                dash.ShowDialog();
+                this.Close();
+            }
+
         }
     }
 }
