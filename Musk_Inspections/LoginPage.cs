34aeab9a63a3851dc68c7118cbd960de1e9e2634
Alex Brain <abrain5423@gmail.com>
2021-11-29 10:33:48 +0000

    Login Functions

--- Musk_Inspections/LoginPage.cs
+++ Musk_Inspections/LoginPage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,19 @@ namespace Musk_Inspections
         public LoginPage()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string query = "INSERT INTO pdf_files (Data,Extension)VALUES(@data,@extn) ";
+
+            using (SqlConnection cn = new SqlConnection(Properties.Settings.Default.Musk_DBConnectionString))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand(query, cn);
+
+                cmd.ExecuteNonQuery();
+            }
         }
     }
 }
