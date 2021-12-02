9eb467bd54bf1604c873ea01a5db8dd7b945d65c
Apostol Apostolov <a.apostolov45@gmail.com>
2021-12-02 12:21:27 +0000

    fix designer for inspectionHSQE

--- Musk_Inspections/inspectionHSQE.Designer.cs
+++ Musk_Inspections/inspectionHSQE.Designer.cs
@@ -1242,7 +1242,8 @@
             this.supervisorBox.TabIndex = 30;
             // 
             // button1
-            // 
+            //
+            /*
             this.button1.Location = new System.Drawing.Point(1123, 138);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 29);
@@ -1250,7 +1251,7 @@
             this.button1.Text = "Save Locally";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
+            // */
             // inspectionHSQE
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
