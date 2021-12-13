b780e055330ff2b6538a3bb8571be360065e990a
FrogzLegz <abrain5423@gmail.com>
2021-12-13 11:26:11 +0000

    Added tempbk, fixed pdf conversion, added rest of f&r

--- Musk_Inspections/inspectionHSQE2.cs
+++ Musk_Inspections/inspectionHSQE2.cs
@@ -57,7 +57,23 @@ namespace Musk_Inspections
                 ref matchControl);
         }
 
-        private void CreateWordDocument(object filename, object SaveAs)
+        private string ReturnChecked(CheckBox cb)
+        {
+            switch (cb.Checked)
+            {
+                case true:
+                    {
+                        return "☑";
+                    }
+                default:
+                case false:
+                    {
+                        return "☐";
+                    }
+            }
+        }
+
+        private void CreateWordDocument(object filename, object SaveAs, object bk)
         {
             Word.Application wordApp = new Word.Application();
             object missing = Missing.Value;
@@ -77,6 +93,8 @@ namespace Musk_Inspections
                 myWordDoc.Activate();
 
                 //find and replace
+
+                // Page 1
                 inspectionHSQE hsqe = pages.HSQE;
                 this.FindAndReplace(wordApp, "<site>", hsqe.cb1.Text);
                 this.FindAndReplace(wordApp, "<workArea>", hsqe.workBox.Text);
@@ -86,63 +104,147 @@ namespace Musk_Inspections
                 this.FindAndReplace(wordApp, "<date>", DateTime.Now.ToShortDateString());
                 this.FindAndReplace(wordApp, "<upP1>", hsqe.upP1.Text);
                 this.FindAndReplace(wordApp, "<upN1>", hsqe.upN1.Text);
+                this.FindAndReplace(wordApp, "<cb1>", ReturnChecked(hsqe.checkBox1));
                 this.FindAndReplace(wordApp, "<comments1>", hsqe.comments2.Text);
                 this.FindAndReplace(wordApp, "<upP2>", hsqe.upP2.Text);
                 this.FindAndReplace(wordApp, "<upN2>", hsqe.upN2.Text);
+                this.FindAndReplace(wordApp, "<cb2>", ReturnChecked(hsqe.checkBox2));
                 this.FindAndReplace(wordApp, "<comments2>", hsqe.comments2.Text);
                 this.FindAndReplace(wordApp, "<upP3>", hsqe.upP3.Text);
                 this.FindAndReplace(wordApp, "<upN3>", hsqe.upN3.Text);
+                this.FindAndReplace(wordApp, "<cb3>", ReturnChecked(hsqe.checkBox3));
                 this.FindAndReplace(wordApp, "<comments3>", hsqe.comments3.Text);
                 this.FindAndReplace(wordApp, "<upP4>", hsqe.upP4.Text);
                 this.FindAndReplace(wordApp, "<upN4>", hsqe.upN4.Text);
+                this.FindAndReplace(wordApp, "<cb4>", ReturnChecked(hsqe.checkBox4));
                 this.FindAndReplace(wordApp, "<comments4>", hsqe.comments4.Text);
                 this.FindAndReplace(wordApp, "<upP5>", hsqe.upP5.Text);
                 this.FindAndReplace(wordApp, "<upN5>", hsqe.upN5.Text);
+                this.FindAndReplace(wordApp, "<cb5>", ReturnChecked(hsqe.checkBox5));
                 this.FindAndReplace(wordApp, "<comments5>", hsqe.comments5.Text);
                 this.FindAndReplace(wordApp, "<upP6>", hsqe.upP6.Text);
                 this.FindAndReplace(wordApp, "<upN6>", hsqe.upN6.Text);
+                this.FindAndReplace(wordApp, "<cb6>", ReturnChecked(hsqe.checkBox6));
                 this.FindAndReplace(wordApp, "<comments6>", hsqe.comments6.Text);
                 this.FindAndReplace(wordApp, "<upP7>", hsqe.upP7.Text);
                 this.FindAndReplace(wordApp, "<upN7>", hsqe.upN7.Text);
+                this.FindAndReplace(wordApp, "<cb7>", ReturnChecked(hsqe.checkBox7));
                 this.FindAndReplace(wordApp, "<comments7>", hsqe.comments7.Text);
                 this.FindAndReplace(wordApp, "<upP8>", hsqe.upP8.Text);
                 this.FindAndReplace(wordApp, "<upN8>", hsqe.upN8.Text);
+                this.FindAndReplace(wordApp, "<cb8>", ReturnChecked(hsqe.checkBox8));
                 this.FindAndReplace(wordApp, "<comments8>", hsqe.comments8.Text);
                 this.FindAndReplace(wordApp, "<upP9>", hsqe.upP9.Text);
                 this.FindAndReplace(wordApp, "<upN9>", hsqe.upN9.Text);
+                this.FindAndReplace(wordApp, "<cb9>", ReturnChecked(hsqe.checkBox9));
                 this.FindAndReplace(wordApp, "<comments9>", hsqe.comments9.Text);
                 this.FindAndReplace(wordApp, "<upP10>", hsqe.upP10.Text);
                 this.FindAndReplace(wordApp, "<upN10>", hsqe.upN10.Text);
+                this.FindAndReplace(wordApp, "<cb10>", ReturnChecked(hsqe.checkBox10));
                 this.FindAndReplace(wordApp, "<comments10>", hsqe.comments10.Text);
                 this.FindAndReplace(wordApp, "<upP11>", hsqe.upP11.Text);
                 this.FindAndReplace(wordApp, "<upN11>", hsqe.upN11.Text);
+                this.FindAndReplace(wordApp, "<cb11>", ReturnChecked(hsqe.checkBox11));
                 this.FindAndReplace(wordApp, "<comments11>", hsqe.comments11.Text);
                 this.FindAndReplace(wordApp, "<upP12>", hsqe.upP12.Text);
                 this.FindAndReplace(wordApp, "<upN12>", hsqe.upN12.Text);
+                this.FindAndReplace(wordApp, "<cb12>", ReturnChecked(hsqe.checkBox12));
                 this.FindAndReplace(wordApp, "<comments12>", hsqe.comments12.Text);
                 this.FindAndReplace(wordApp, "<upP13>", hsqe.upP13.Text);
                 this.FindAndReplace(wordApp, "<upN13>", hsqe.upN13.Text);
+                this.FindAndReplace(wordApp, "<cb13>", ReturnChecked(hsqe.checkBox13));
                 this.FindAndReplace(wordApp, "<comments13>", hsqe.comments13.Text);
                 this.FindAndReplace(wordApp, "<upP14>", hsqe.upP14.Text);
                 this.FindAndReplace(wordApp, "<upN14>", hsqe.upN14.Text);
+                this.FindAndReplace(wordApp, "<cb14>", ReturnChecked(hsqe.checkBox14));
                 this.FindAndReplace(wordApp, "<comments14>", hsqe.comments14.Text);
                 this.FindAndReplace(wordApp, "<upP15>", hsqe.upP15.Text);
                 this.FindAndReplace(wordApp, "<upN15>", hsqe.upN15.Text);
+                this.FindAndReplace(wordApp, "<cb15>", ReturnChecked(hsqe.checkBox15));
                 this.FindAndReplace(wordApp, "<comments15>", hsqe.comments15.Text);
+
+                // Page 2 -- ALL FIELDS THERE BUT NUMBERS NOT CHANGED**
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
+                this.FindAndReplace(wordApp, "<upP16>", upP16.Text);
+                this.FindAndReplace(wordApp, "<upN16>", upN16.Text);
+                this.FindAndReplace(wordApp, "<cb16>", ReturnChecked(checkBox16));
+                this.FindAndReplace(wordApp, "<comments16>", comments16.Text);
             }
             else
             {
                 MessageBox.Show("File not Found!");
             }
-            myWordDoc.SaveAs2(ref SaveAs, ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing,
-                           ref missing, ref missing, ref missing);
 
-            myWordDoc.Close();
-            wordApp.Quit();
-            MessageBox.Show("File Created!");
+            try
+            {
+                myWordDoc.SaveAs2(ref SaveAs, Word.WdSaveFormat.wdFormatPDF, ref missing, ref missing,
+                               ref missing, ref missing, ref missing,
+                               ref missing, ref missing, ref missing,
+                               ref missing, ref missing, ref missing,
+                               ref missing, ref missing, ref missing);
+
+                myWordDoc.Close();
+
+
+                // Replace Temp.docx with backup so is not overridden
+
+                myWordDoc = wordApp.Documents.Open(bk, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing,
+                                        ref missing, ref missing, ref missing, ref missing);
+
+                myWordDoc.Activate();
+
+                myWordDoc.SaveAs2(ref filename, ref missing, ref missing,
+                               ref missing, ref missing, ref missing,
+                               ref missing, ref missing, ref missing,
+                               ref missing, ref missing, ref missing,
+                               ref missing, ref missing, ref missing);
+
+                myWordDoc.Close();
+
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Your version of Microsoft Word or Interop.Word.nupkg may be out of date.\nPlease verify versions and try again.");
+            }
+            finally
+            {
+                wordApp.Quit();
+                MessageBox.Show("File Created!");
+            }
         }
 
        
@@ -165,14 +267,16 @@ namespace Musk_Inspections
             string fpMain = Dashboard.Directories.mainDirectory;
             string filePathWord = Path.Combine(fpMain, "Temp.docx");
 
+            string filePathWordBK = Path.Combine(fpMain, "TempBK.docx");
+
             //create the path for the new file
-            string fpWord = Dashboard.Directories.dirWordFile;
-            string newWordFilePath = Path.Combine(fpWord, inspectionId + ".docx");
+            string fpPDF = Dashboard.Directories.dirPDFfile;
+            string newPDFFilePath = Path.Combine(fpPDF, inspectionId + ".pdf");
 
             string filePath = Dashboard.Directories.dirPDFfile;
             filePath = Path.Combine(filePath, "test.pdf");
 
-            CreateWordDocument(filePathWord, newWordFilePath);
+            CreateWordDocument(filePathWord, newPDFFilePath, filePathWordBK);
 
             SaveFileIntoDatabase(filePath);
             inspectionIntoDatabase();
@@ -276,7 +380,8 @@ namespace Musk_Inspections
                 cmd.Parameters.Add("@PDF_file", SqlDbType.Int ).Value = inspectionId + 1;
 
                 cn.Open();
-                cmd.ExecuteNonQuery();
+                try { cmd.ExecuteNonQuery(); }
+                catch (Exception e) {};
             }
         }
 
